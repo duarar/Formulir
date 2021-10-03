2827c09d9fdc159057954258b10a4ee8b9c407b4
Triandi <86074088+triandii@users.noreply.github.com>
2021-10-03 21:48:42 +0700

    Menambahkan Alert , memperbaiki Reset

--- assets/js/spredsheet.js
+++ assets/js/spredsheet.js
@@ -1,9 +1,22 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbx9zIjoIOAe_aoZPoAPXRBfuzhBZeP6dM6M_-f4iv0Xvm3PB_bs3agDtC9eorFU6MVH/exec';
 const form = document.forms['formulir-pendaftaran-cc'];
+const btnSubmit = document.querySelector('.btn-submit');
+const btnLoading = document.querySelector('.btn-loading');
+const myAlert = document.querySelector('.my-alert');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  btnLoading.classList.toggle('d-none');
+  btnSubmit.classList.toggle('d-none');
   fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-    .then((response) => console.log('Success!', response))
+    .then((response) => {
+      // tampilkan tombol kirin , hilangkan tombol loading
+      btnLoading.classList.toggle('d-none');
+      btnSubmit.classList.toggle('d-none');
+      // tampilkan alert
+      myAlert.classList.toggle('d-none');
+      form.reset();
+      console.log('Success!', response);
+    })
     .catch((error) => console.error('Error!', error.message));
 });
